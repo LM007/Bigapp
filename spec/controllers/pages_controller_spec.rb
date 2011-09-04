22e45064c3b7bc22a3c5a85060c0dffae20a97cd
leemarshall <lpdmarshal@gmail.com>
2011-09-04 10:58:12 +0100

    competed chap 3, static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
- render_views
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "BigApp|Home")
     end
   end
 
@@ -15,14 +21,29 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content =>
+                          "BigApp|Contact")
+    end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "BigApp|About")
+    end
   end
-
-
 end
+
+
+
